2ae044bc4e3e9165ac73055b0fea44dafa67f2b1
MlnrGyrgy <molnar.gyorgy.mrgr@gmail.com>
2020-09-07 11:43:34 +0200

    Add project files.

---
+++ ProbaGit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a < 10; a++)
+            {
+                Console.WriteLine("Yes");
+            }
+            Console.ReadKey();
+        }
+    }
+}
